01b1405af0837ca672fe6bd49bd431995ca33c5d
alex jiang <alexjiang201507@gmail.com>
2025-01-28 03:17:03 +1100

    Hello World with React js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
+        react(),
     ],
+    server: {
+        host: '192.168.33.77',
+        https: false,
+        cors: true,
+        hmr: {
+            host: '192.168.33.77',
+        }
+    },
 });
