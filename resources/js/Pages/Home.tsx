16e372cc44956a586e011731f0e19145d56d340c
alex jiang <alexjiang201507@gmail.com>
2025-01-28 20:51:36 +1100

    Setup MUI

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 const Home = () => {
-    return <div>Home</div>;
+    return <Button>Hello World</Button>;
 };
 
 export default Home;
