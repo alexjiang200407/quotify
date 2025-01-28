630a03fcdbfa9e7ddd59f30e3e598c80f9c05cff
alex jiang <alexjiang201507@gmail.com>
2025-01-28 20:51:50 +1100

    Fix vite not watching file changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: '/resources',
     plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.tsx"],
-            refresh: true,
-        }),
-        react(),
+      react(),
+      laravel({
+        input: [
+          "resources/css/app.css",
+          "resources/js/app.tsx"
+        ],
+        refresh: [
+          'resources/**',
+          'routes/**',
+          'resources/views/**',
+      ],
+      }),
     ],
     server: {
         host: '192.168.33.77',
@@ -16,6 +24,10 @@ export default defineConfig({
         cors: true,
         hmr: {
             host: '192.168.33.77',
-        }
+        },
+        watch: {
+          usePolling: true,
+          interval: 1000,
+        },
     },
 });
