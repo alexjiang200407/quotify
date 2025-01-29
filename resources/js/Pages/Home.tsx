9adb55cdf1bc67c272bd3d6423c6b9bde398466a
Andrew Gong <z5479801@ad.student.unsw.edu.au>
2025-01-29 22:59:22 +1100

    finished home page

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Card, CardContent } from "@mui/material";
+import { Box, Typography, Card, CardContent, Chip, Divider } from "@mui/material";
 import { faHeart as solidHeart, faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart, faBookmark as regularBookmark } from '@fortawesome/free-regular-svg-icons';
 import { IconButton } from "../Components/IconButton";
@@ -9,6 +9,8 @@ const Home = () => {
     const author = "William Shakespeare";
     const likeCount = 120;
     const saveCount = 45;
+    const isMobile = true;
+    const tags = ["Philosophy", "Poetry", "Renaissance"];
 
     return (
         <Box
@@ -18,47 +20,58 @@ const Home = () => {
                 justifyContent: "center",
                 alignItems: "center",
                 height: "100vh",
-                padding: 2,
             }}
         >
-            {/* Title */}
-            <Typography variant="h6" gutterBottom sx={{ textAlign: "center", marginBottom: 2 }}>
-                Quote of the Day
-            </Typography>
-
-            {/* Quote Card */}
-            <Card sx={{ display: "flex", maxWidth: 600, boxShadow: "none" }}>
-                {/* Icons */}
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: 0,
-                        padding: 2,
-                        marginRight: 2,
-                        alignItems: "center",
-                    }}
-                >
+            {/* Container Box */}
+            <Box sx={{ display: "flex", flexDirection: isMobile ? "row" : "column", alignItems: "center", gap: 2 }}>
+                {/* Icons Box */}
+                <Box sx={{ display: "flex", flexDirection: isMobile ? "column" : "row", gap: 1, alignItems: "center" }}>
                     <IconButton icon={regularHeart} solidIcon={solidHeart} activeColor="red" defaultColor="black" />
                     <Typography variant="caption">{likeCount}</Typography>
                     <IconButton icon={regularBookmark} solidIcon={solidBookmark} activeColor="blue" defaultColor="black" />
                     <Typography variant="caption">{saveCount}</Typography>
                 </Box>
 
-                {/* Quote Content */}
-                <CardContent sx={{ textAlign: "left", flex: 1 }}>
-                    {quote.split('\n').map((line, index) => (
-                        <Typography key={index} variant="body1" paragraph>
-                            {line}
-                        </Typography>
-                    ))}
-                </CardContent>
-            </Card>
+                {/* Content Box */}
+                <Box sx={{ display: "flex", flexDirection: "column", maxWidth: 600 }}>
+                    {/* Title */}
+                    <Typography variant="h6" gutterBottom sx={{ textAlign: "center" }}>
+                        Quote of the Day
+                    </Typography>
 
-            {/* Author Name */}
-            <Typography variant="h6" sx={{ textAlign: "center"}}>
-                {author}
-            </Typography>
+                    {/* Animated Divider */}
+                    <Divider className="animated-divider" />
+
+                    {/* Quote */}
+                    <Card sx={{ boxShadow: "none", marginTop: 2 }}>
+                        <CardContent>
+                            {quote.split('\n').map((line, index) => (
+                                <Typography key={index} variant="body1" paragraph>
+                                    {line}
+                                </Typography>
+                            ))}
+                        </CardContent>
+                    </Card>
+
+                    {/* Tags */}
+                    <Box sx={{ display: "flex", gap: 1, marginTop: 1 }}>
+                        {tags.map((tag, index) => (
+                            <Chip
+                                key={index}
+                                label={tag}
+                                size="small"
+                                sx={{ fontSize: "0.75rem", cursor: "pointer" }}
+                                onClick={() => alert(`Clicked on ${tag}`)} // Replace this with actual navigation or filtering logic
+                            />
+                        ))}
+                    </Box>
+
+                    {/* Author Name */}
+                    <Typography variant="h6" sx={{ textAlign: "center", marginTop: 2 }}>
+                        {author}
+                    </Typography>
+                </Box>
+            </Box>
         </Box>
     );
 };
