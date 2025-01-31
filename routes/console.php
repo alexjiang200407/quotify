fce539aaa3cc37b9cac9018b65bcbf271364dc90
alex jiang <alexjiang201507@gmail.com>
2025-01-31 22:50:17 +1100

    Update Daily Quote daily

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('quote:update-daily', function () {
+    DB::statement('CALL update_daily_quote()');
+})->purpose('Update the daily quote using the stored procedure');
+
+Schedule::command("quote:update-daily")->dailyAt("23:59");
