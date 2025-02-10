9f21af4a5a7683ec8db4d53d18322f2ef729bd41
alex jiang <alexjiang201507@gmail.com>
2025-02-10 22:57:56 +1100

    Register signature script

--- scripts/psvupdate.py
+++ scripts/psvupdate.py
@@ -16,6 +16,7 @@ def getexistingid(cursor, table, col, op, val):
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--authors-file', type=argparse.FileType('r', encoding="utf8"), required=True)
 parser.add_argument('-q', '--quotes-file', type=argparse.FileType('r', encoding="utf8"), required=True)
+parser.add_argument('-s', '--signatures-file', type=argparse.FileType('r', encoding="utf8"), required=True)
 
 args = parser.parse_args()
 
@@ -29,16 +30,20 @@ connection = pymysql.connect(host='localhost',
 
 tagids = {}
 authorids = {}
+tagsignatureids = {}
+authorsignatureids = {}
 
 with connection, connection.cursor() as cursor:
     set_foreign_key_checks_sql = "SET FOREIGN_KEY_CHECKS = %s"
 
     try:
-        cursor.execute("INSERT INTO signatures (type) VALUES ('default')")
-        signatureid = getlastid(cursor)
+        cursor.execute("INSERT INTO signatures (`type`, `created_at`, `updated_at`) VALUES ('default', CURRENT_TIME(), CURRENT_TIME())")
+        defaultsignatureid = getlastid(cursor)
     except Exception as e:
-        signatureid = getexistingid(cursor, 'signatures', 'type', '=', 'default')
+        defaultsignatureid = getexistingid(cursor, 'signatures', 'type', '=', 'default')
 
+    signature_inserted = 0
+    signature_existing = 0
     authors_inserted = 0
     quotes_inserted = 0
     tags_inserted = 0
@@ -46,15 +51,43 @@ with connection, connection.cursor() as cursor:
     quotes_existing = 0
     tags_existing = 0
 
+    for line in args.signatures_file:
+        line = line.rstrip()
+        [signaturetype, font_sz, stroke_width, letter_spacing, duration, color, authors] = line.split('|')
+        try:
+            cursor.execute("""
+                INSERT INTO signatures (`type`, `font_size`, `stroke_width`, `letter_spacing`, `duration`, `color`, `created_at`, `updated_at`)
+                VALUES (%s, %s, %s, %s, %s, %s, CURRENT_TIME(), CURRENT_TIME())
+            """, (signaturetype, font_sz, stroke_width, letter_spacing, duration, color))
+
+            lastid = getlastid(cursor)
+
+            print(f"Signature '{signaturetype}' inserted...")
+            signature_inserted += 1
+
+        except Exception as e:
+            print(f"Signature '{signaturetype}' already exists finding previous ID...")
+
+            lastid = getlastid(cursor)
+            signature_existing += 1
+
+        for author in authors.split(','):
+            authorsignatureids[author] = lastid
+
     for line in args.authors_file:
         line = line.rstrip()
         [author, wiki_link, description] = line.split('|')
 
         try:
+            if author in authorsignatureids:
+                signatureid = authorsignatureids[author]
+            else:
+                signatureid = defaultsignatureid
             cursor.execute("""
-                INSERT INTO authors (`full_name`, `wiki_page`, `description`, `signature_id`)
-                VALUES (%s, %s, %s, %s)
+                INSERT INTO authors (`full_name`, `wiki_page`, `description`, `signature_id`, `created_at`, `updated_at`)
+                VALUES (%s, %s, %s, %s, CURRENT_TIME(), CURRENT_TIME())
             """, (author, wiki_link, description, signatureid))
+
             authorids[author] = getlastid(cursor)
             print(f"Author '{author}' inserted...")
             authors_inserted += 1
@@ -113,6 +146,8 @@ with connection, connection.cursor() as cursor:
     print(f"Found {quotes_existing} quotes")
     print(f"Added {tags_inserted} tags")
     print(f"Found {tags_existing} existing tags")
+    print(f"Added {signature_inserted} signatures")
+    print(f"Found {signature_existing} existing signatures")
 
     connection.commit()
 
