d5f1182eb36300d9012e8df7dfb5c94ab1b28091
alex jiang <alexjiang201507@gmail.com>
2025-02-13 20:27:39 +1100

    Change Title and Favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-        <title>Laravel 9 vite with react</title>
+        <title>Quotify</title>
     
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
